3d79c84d5f1daa33e904b0e35e3698f048b0598e
Amy Zi MacLean Stephen <azstephe@lanec1.compbio.cs.cmu.edu>
2025-06-05 10:33:12 -0400

    writing manuscript

--- memechip.sh
+++ memechip.sh
@@ -2,5 +2,8 @@
 
 outdir=$1
 infasta=$2
+backgroundfasta=$3
 
-/home/ikaplow/anaconda2/bin/meme-chip -o $outdir -db /projects/pfenninggroup/machineLearningForComputationalBiology/regElEvoGrant/MotifData/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme $infasta
+/home/ikaplow/anaconda2/bin/meme-chip -o $outdir -db /projects/pfenninggroup/machineLearningForComputationalBiology/regElEvoGrant/MotifData/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme -neg $backgroundfasta $infasta
+
+#/home/ikaplow/anaconda2/bin/meme-chip -o $outdir -db /projects/pfenninggroup/machineLearningForComputationalBiology/regElEvoGrant/MotifData/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme $infasta
